d171ced829beb51928128d2d2f97730c81f43b96
Tradon <tradon@praneat.com>
2025-02-16 19:08:57 +0700

    feat: 🎸 init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common/utils/mongo-exception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new GlobalExceptionFilter());
+  const config = new DocumentBuilder()
+    .setTitle('Workshop Backend NestJs')
+    .setDescription('NestJs REST api created by Tradon')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, documentFactory, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
