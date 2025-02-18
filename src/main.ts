0b5270e512a8f8839ffed29a982ea353655a1161
Tradon <tradon@praneat.com>
2025-02-18 11:23:47 +0700

    fix: 🐛 change to port 8000

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
     jsonDocumentUrl: 'swagger/json',
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
