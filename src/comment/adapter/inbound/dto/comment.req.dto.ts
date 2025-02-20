6a15b264dd0fe71c1b21a3b6774706602413e479
Tradon <tradon@praneat.com>
2025-02-21 00:11:49 +0700

    fix: 🐛 fix wrong import

--- src/comment/adapter/inbound/dto/comment.req.dto.ts
+++ src/comment/adapter/inbound/dto/comment.req.dto.ts
@@ -1,4 +1,4 @@
-import { IComment } from '@/Comment/interface/domain/comment.domain';
+import { IComment } from '@/comment/interface/domain/comment.domain';
 import { ApiProperty } from '@nestjs/swagger';
 
 import { IsNotEmpty, IsString } from 'class-validator';
