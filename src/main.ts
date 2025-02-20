b18402e944d07c55869cc8dc964142de0183e64a
Tradon <tradon@praneat.com>
2025-02-20 23:33:33 +0700

    fix: 🐛 update env

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { configEnv } from './common/configs/configEnv.config';
 import { GlobalExceptionFilter } from './common/utils/mongoException';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const { frontendWebDomainProd, frontendWebDomainDev } = configEnv();
+  app.use(cookieParser());
   app.useGlobalFilters(new GlobalExceptionFilter());
-  app.enableCors();
+  app.enableCors({
+    origin: [frontendWebDomainProd, frontendWebDomainDev],
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Workshop Backend NestJs')
     .setDescription('NestJs REST api created by Tradon')
