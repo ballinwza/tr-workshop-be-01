192898bee59e9994c9a9b6dcd4b6f316d410f873
Tradon <tradon@praneat.com>
2025-02-20 23:25:56 +0700

    fix: 🐛 update dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
 import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
 import { CommentModule } from './comment/comment.module';
 import mongooseConfig from './common/configs/mongoose.config';
 import { ConfigName } from './common/enums/configName.enum';
@@ -22,6 +24,19 @@ import { UserModule } from './user/user.module';
         ...configService.get(ConfigName.MongooseConfig),
       }),
     }),
+    JwtModule.registerAsync({
+      global: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        global: true,
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRES_IN'),
+        },
+      }),
+    }),
+    AuthModule,
     UserModule,
     CommentModule,
     PlacardModule,
