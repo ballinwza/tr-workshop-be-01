542180e4a67f6f7d7831ae8b01e26d2d4b2218a7
Tradon <tradon@praneat.com>
2025-02-21 00:16:48 +0700

    fix: 🐛 fix dependencies error

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -6,7 +6,7 @@ import {
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectModel } from '@nestjs/mongoose';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { Model } from 'mongoose';
 
 @Injectable()
