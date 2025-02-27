671f7a140744c618d79e4cd81d4b2038ceaf3d53
Tradon <tradon@praneat.com>
2025-02-27 23:42:30 +0700

    fix: 🐛 fix get auth from cookie directly

--- src/guard/jwt/jwt.strartegy.ts
+++ src/guard/jwt/jwt.strartegy.ts
@@ -1,6 +1,7 @@
 import { configEnv } from '@/common/configs/configEnv.config';
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { IJwtPayload } from './jwt.payload.interface';
 
@@ -9,7 +10,12 @@ export const StrategyJwtName = 'jwt';
 export class JwtStrategy extends PassportStrategy(Strategy, StrategyJwtName) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      // jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          return request?.cookies.access_token || null;
+        },
+      ]),
       ignoreExpiration: false,
       secretOrKey: configEnv().jwtSecret,
     });
